7c8f83f46dcf825d8025bede5134a755ce479fdb
crescent <crescent@supernova.ipv6.infinitum.net.mx>
2026-08-17 22:45:15 -0500

    first ex. | NOT FUNCTIONAL

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import mariadb
 from netmiko import ConnectHandler
 
 #connect db
-db_config = {
+connector = mariadb.connect{
         'host': 'localhost',
         'port': 3306,
         'user': 'your_username',
         'password': 'your password',
         'database': 'automated_network_config'
 }
+
+# create cursor
+cursor = connector.cursor()
+#cursor.execute("query")
+#connector.commit()
+#cursor.close()
+#connector.close()
 
 cisco_2900 = {
     'device_type' = 'cisco_ios'
@@ -19,8 +26,16 @@ cisco_2900 = {
     'secret' = 'cisco'
 }
 
+#DEVICE INFORMATION ***** STATIC FOR NOW FOR ALL DEVICES RUNNING SCRIPT DIFFERENT VALUES
+cursor.execute("INSERT INTO source_device (vendor_name, device_name, device_ip) VALUES (?,?,?)", ("ASUS","crescent","192.168.1.2"))
+
+#NETWORK DEVICE INFORMATION ***** STATIC ALWAYS
+cursor.execute("INSERT INTO network_device (vendor_name, device_layer, device_ip) VALUES (?,?,?)", ("CISCO","2","192.168.1.253"))
+
+##CREATE CONNECTION
+
 net_connect = ConnectHandler(**cisco_2900)
 
-int_brief_output = net_connect.send_command('show interfaces brief')
-
-
+int_status_command = net_connect.send_command('show int stat')
+#print(int_status_command)
+cusror.execute("INSERT INTO instance (source_id, net_device_id, command, output_text) VALUES (?,?,?,?)", ())
